16c96c4ad6ab32447b40aa5ac63f55181828a73a
Richard Swainson <rich_swainson@hotmail.co.uk>
2023-01-20 10:53:00 +0000

    Add round function

---
+++ game.js
@@ -0,0 +1,47 @@
+function startRound(board) {
+  // go through the list of winning lines
+  // if all the indexes of that line in array are Xs or Os, game over
+  for (line of this.winningLines) {
+    let count = {
+      x: 0,
+      o: 0,
+    };
+    for (index of line) {
+      let element = this.board[index].toLowerCase();
+      count[element]++;
+    }
+    let winner;
+    if (count.x === 3 || count.o === 3) {
+      if (count.x === 3) {
+        winner = "X";
+      } else {
+        winner = "O";
+      }
+      return {
+        finished: true,
+        winner: winner,
+      };
+    }
+    return { finished: false };
+  }
+}
+
+// array representing the board
+board = ["O", "X", "X", ".", ".", ".", ".", ".", "."];
+// each array in winningLines represents a possible line to win
+winningLines = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2],
+];
+
+result = { finished: false };
+console.log(startRound(board));
+// while (!result.finished){
+//     result = startRound(board);
+// }
