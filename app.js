b6e9efb43fde25159a988978a7fafa9f5020b779
Brandon <brandon.wdbiz@gmail.com>
2023-01-20 11:09:41 +0000

    add turn taking

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const tiles = document.querySelectorAll(".clickable-tile")
+let playerTurn = 1
+
+function takeTurn(element) {
+    if (playerTurn === 1) {
+        element.target.setAttribute("style", "background-image: url('./assets/cross.png')")
+        playerTurn = 2
+    } else {
+        element.target.background = element.target.setAttribute("style", "background-image: url('./assets/nought.png')")
+        playerTurn = 1
+    }
+    element.target.removeEventListener("click", takeTurn)
+}
 
 function initializeBoard() {
     let board = []
     for (let i = 0; i < tiles.length; i++) {
         board[i] = tiles[i]
+        board[i].addEventListener("click", takeTurn)
     }
     return board;
 }
