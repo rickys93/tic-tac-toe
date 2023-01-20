940595b88bed35de24bcf6038e8ae3352e5370ee
Brandon <brandon.wdbiz@gmail.com>
2023-01-20 10:23:17 +0000

    simpler board array

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const tiles = document.querySelectorAll(".clickable-tile")
 
 function initializeBoard() {
-    let board = [[],[],[]]
+    let board = []
     for (let i = 0; i < tiles.length; i++) {
-        board[Math.floor(i / 3)][Math.floor(i % 3)] = tiles[i]
+        board[i] = tiles[i]
     }
     return board;
 }
 
-console.log(initializeBoard())
+const board = initializeBoard()
